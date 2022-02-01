a334485c03681c3354e29adc1cab84b0906cf9dd
Nuri1977 <nurilacka@gmail.com>
2022-02-01 11:18:50 +0100

    Display list of items on the Homepage

---
+++ src/modules/render-home.js
@@ -0,0 +1,24 @@
+const renderShows = (shows) => {
+  const showsContainer = document.getElementById('shows-list');
+  showsContainer.innerHTML = '';
+  shows.forEach((show) => {
+    const listItem = `
+    <li>
+      <img src="${show.image.medium}" alt="show">
+      <div class="show-info">
+        <h3>${show.name}</h3>
+        <div class="show-likes">
+          <i id="${show.id}" class="far fa-heart"></i>
+          <h4>0 likes</h4>
+        </div>
+      </div>
+      <div class="show-btn">
+        <button type="button" data-id="${show.id}"  class="btn btn-comment">Comments</button>
+      </div>
+    </li>
+    `;
+    showsContainer.innerHTML += listItem;
+  });
+};
+
+export default renderShows;
