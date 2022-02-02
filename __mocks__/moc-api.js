e884be1064c870a14f8edafe4ff9c59ab54b41a0
Nuri1977 <nurilacka@gmail.com>
2022-02-02 13:15:36 +0100

    Add testing for counting movies

---
+++ __mocks__/moc-api.js
@@ -0,0 +1,52 @@
+const shows1 = [
+  {
+    id: 2345,
+    name: 'Ocean eleven',
+    description: 'great movie',
+  },
+  {
+    id: 2569,
+    name: 'Dune',
+    description: 'great movie',
+  },
+  {
+    id: 6589,
+    name: 'Godfather 1',
+    description: 'great movie',
+  },
+  {
+    id: 4586,
+    name: 'Lord of the Rings 1',
+    description: 'great movie',
+  },
+];
+
+const shows2 = [
+  {
+    id: 2345,
+    name: 'Ocean eleven',
+    description: 'great movie',
+  },
+  {
+    id: 2569,
+    name: 'Dune',
+    description: 'great movie',
+  },
+  {
+    id: 6589,
+    name: 'Godfather 1',
+    description: 'great movie',
+  },
+  {
+    id: 4586,
+    name: 'Lord of the Rings 1',
+    description: 'great movie',
+  },
+  {
+    id: 4858,
+    name: 'Green Book',
+    description: 'great movie',
+  },
+];
+
+export { shows1, shows2 };
---
+++ src/modules/show-api.test.js
@@ -0,0 +1,13 @@
+import { countMovies } from './shows-api.js';
+import { shows1, shows2 } from '../../__mocks__/moc-api.js';
+
+describe('Testing count of movies', () => {
+  test('Count of movies should be 4', () => {
+    const moviesCount = countMovies(shows1);
+    expect(moviesCount).toBe(4);
+  });
+  test('Count of movies should be 5', () => {
+    const moviesCount = countMovies(shows2);
+    expect(moviesCount).toBe(5);
+  });
+});
